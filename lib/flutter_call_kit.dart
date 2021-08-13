091f09e7c49386ec2e37ea1966f27ef1773629e9
Dokook Choe <cdg720@gmail.com>
2021-08-13 15:20:16 -0700

    remove prints

--- lib/flutter_call_kit.dart
+++ lib/flutter_call_kit.dart
@@ -184,8 +184,6 @@ class FlutterCallKit {
   }
 
   Future<dynamic> _handleMethod(MethodCall call) async {
-    print(call);
-    print(call.method);
     switch (call.method) {
       case "didReceiveStartCallAction":
         if (_didReceiveStartCallAction == null) {
@@ -226,7 +224,6 @@ class FlutterCallKit {
           return null;
         }
         Map map = call.arguments.cast<String, dynamic>();
-        print(map);
         return _didDisplayIncomingCall!(map["error"], map["callUUID"],
             map["handle"], map["localizedCallerName"], map["fromPushKit"]);
       case "didPerformSetMutedCallAction":
